379dab5f25aef7984a141f4d315d66ec3f8009e0
George Quarelli <quarelli.george@gmail.com>
2020-06-17 18:43:16 +0200

    inizio a stilizare la versione php

--- versione-php/index.php
+++ versione-php/index.php
@@ -7,16 +7,25 @@
     <link rel="stylesheet" href="dist/app.css">
   </head>
   <body>
+    <header>
+      <div class="container">
+          <img src="img/logo.png" alt="logo" />
+      </div>
+    </header>
 
-    <div class="container_card">
-    <?php foreach ($dischi as $disco) { ?>
-      <img src="<?php echo $disco['poster']; ?>" alt="cover">
-      <h3><?php echo $disco['title']; ?></h3>
-      <span><?php echo $disco['author']; ?></span>
-      <span><?php echo $disco['year']; ?></span>
-      <span><?php echo $disco['genre']; ?></span>
-    <?php } ?>
-    </div>
+    <main>
+      <div class="card_container">
+        <div class="card">
+          <?php foreach ($dischi as $disco) { ?>
+            <img src="<?php echo $disco['poster']; ?>" alt="cover">
+            <h3><?php echo $disco['title']; ?></h3>
+            <p class="author"><?php echo $disco['author']; ?></p>
+            <p class="year"><?php echo $disco['year']; ?></p>
+            <p class="genre"><?php echo $disco['genre']; ?></p>
+          <?php } ?>
+        </div>
+      </div>
+    </main>
 
     <script src="src/app.js" charset="utf-8"></script>
   </body>
